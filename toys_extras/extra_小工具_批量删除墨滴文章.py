9e2fc253e875eaff62eb89eafbae7148af92d4e7
toysextra <toysextra@tutamail.com>
2025-01-05 17:29:28 +0800

    Update extra_小工具_批量删除墨滴文章.py

--- toys_extras/extra_小工具_批量删除墨滴文章.py
+++ toys_extras/extra_小工具_批量删除墨滴文章.py
@@ -1,9 +1,11 @@
 from toys_logger import logger
 from toys_extras.articles import Articles
 from playwright.sync_api import Page
+from toys_utils import ToyError
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
+# 切换文件夹方法优化
 
 class Toy(Articles):
 
@@ -12,18 +14,36 @@ class Toy(Articles):
         self.url = "https://editor.mdnice.com/"
         self.result_table_view: list = [['文章标题', '状态']]
 
+    def choose_catalog(self, catalog_name: str, depth: int = 1) -> None:
+        print(depth)
+        if depth == 5:
+            raise ToyError("选择墨滴文件夹失败，请确认文件夹名称是否正确")
+        try:
+            catalog_btn = self.page.locator(".catalog-btn")
+            item_list = self.page.locator(".ant-list")
+            catalog_list = self.page.locator(".catalog-sidebar-list-item-container")
+            item_list.wait_for(state="attached")
+            try:
+                item_list.locator(".ant-list-items").wait_for(timeout=3000)
+            except Exception:
+                pass
+            if catalog_list.count():
+                self.page.locator(".catalog-name", has_text=catalog_name).evaluate("element => element.click()")
+                self.random_wait(1000, 1500)
+            elif catalog_btn.text_content() != catalog_name:
+                catalog_btn.click(timeout=3000)
+                self.random_wait(1000, 1500)
+                self.page.locator(".catalog-name", has_text=catalog_name).evaluate("element => element.click()")
+                self.random_wait(1000, 1500)
+        except Exception as e:
+            logger.exception(f"Error: {e}")
+            self.choose_catalog(catalog_name, depth+1)
+
     def play(self):
         catalog_name = self.config.get("扩展", "墨滴文件夹")
         self.navigate()
         # 切换文章文件夹
-        catalog_btn = self.page.locator(".catalog-btn")
-        new_catalog_btn = self.page.get_by_role("button", name="新增文件夹")
-        catalog_btn.or_(new_catalog_btn).wait_for()
-        if catalog_btn.count() and catalog_btn.text_content() != catalog_name:
-            catalog_btn.click()
-            self.page.locator(".catalog-name", has_text=catalog_name).click()
-        elif new_catalog_btn.count():
-            self.page.locator(".catalog-name", has_text=catalog_name).click()
+        self.choose_catalog(catalog_name)
         first_article = self.page.locator(".ant-list-items .ant-list-item").first
         first_article.wait_for()
         while first_article.is_visible(timeout=5000):
