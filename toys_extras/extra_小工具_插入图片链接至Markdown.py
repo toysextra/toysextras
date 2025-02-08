468f128b85e65da695acbd3a8b861b30e6bd2260
toysextra <toysextra@tutamail.com>
2025-02-08 15:39:31 +0800

    Update extra_小工具_插入图片链接至Markdown.py

--- toys_extras/extra_小工具_插入图片链接至Markdown.py
+++ toys_extras/extra_小工具_插入图片链接至Markdown.py
@@ -1,10 +1,10 @@
 import os
 import random
-from toys_utils import split_markdown_to_paragraphs
+from toys_utils import insert_image_link_to_markdown
 from toys_extras.base import Base
 from toys_logger import logger
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 class Toy(Base):
@@ -12,31 +12,10 @@ class Toy(Base):
     def __init__(self):
         super().__init__()
         self.result_table_view: list = [["文件", "状态", "输出目录"]]
-
-    @staticmethod
-    def insert_image_link_to_markdown(file, image_links):
-        with open(file, "r", encoding="utf-8") as f:
-            markdown_text = f.read()
-        paragraphs = split_markdown_to_paragraphs(markdown_text)
-        num_paragraphs = len(paragraphs)
-
-        if num_paragraphs > len(image_links):
-            insert_num = len(image_links)
-        else:
-            insert_num = num_paragraphs - 1
-        first_insert_index = num_paragraphs // (insert_num + 1)
-        # 其他图片位置间隔为段落数除以插入数量四舍五入
-        other_insert_interval = round(num_paragraphs / (insert_num + 1))
-        insert_index = [first_insert_index]
-        for i in range(1, insert_num):
-            insert_index.append(first_insert_index + i * other_insert_interval)
-        for i, index in enumerate(insert_index):
-            paragraphs[index] = f"{paragraphs[index]}\n\n![]({image_links[i]})"
-        return "\n\n".join(paragraphs)
-
     def play(self):
         图片链接文件名 = self.config.get("扩展", "图片链接文件名")
         插入数量 = int(self.config.get("扩展", "插入数量"))
+        插图位置 = self.config.get("扩展", "插图位置")
         图片链接文件数量 = len([file for file in self.files if os.path.basename(file) == f"{图片链接文件名}"])
         if 图片链接文件数量 == 0:
             logger.error(f"未找到{图片链接文件名}文件")
@@ -59,7 +38,7 @@ class Toy(Base):
                     image_links = random.sample(image_links, 插入数量)
                 else:
                     image_links = image_links[:插入数量]
-            markdown_text = self.insert_image_link_to_markdown(file, image_links)
+            markdown_text = insert_image_link_to_markdown(file, image_links)
             output_file = os.path.join(output_dir, os.path.basename(file))
             with open(output_file, "w", encoding="utf-8") as f:
                 f.write(markdown_text)
