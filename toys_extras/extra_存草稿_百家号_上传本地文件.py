6b5915f025565cc521dbf91edad13e101bdaeea7
toysextra <toysextra@tutamail.com>
2025-10-08 20:53:23 +0800

    Update extra_存草稿_百家号_上传本地文件.py

--- toys_extras/extra_存草稿_百家号_上传本地文件.py
+++ toys_extras/extra_存草稿_百家号_上传本地文件.py
@@ -1,7 +1,7 @@
 from toys_extras.articles import Articles
 from playwright.sync_api import Page
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 class Toy(Articles):
@@ -9,7 +9,10 @@ class Toy(Articles):
     def __init__(self, page: Page):
         super().__init__(page)
         self.url = "https://baijiahao.baidu.com/builder/rc/edit?type=news&is_from_cms=1"
-        self.button_导入文档 = self.page.locator("div.edui-for-importdoc")
+        self.文章标题输入框 = self.page.get_by_placeholder("请输入标题（2 - 64字）")
+        self.文章第1行 = self.page.frame_locator("iframe[id='ueditor_0']").locator(".view.news-editor-pc p").first
+        self.hove_导入文档 = self.page.locator(".edui-for-bjhInsertionDrawer")
+        self.button_导入文档 = self.page.get_by_text("导入文档")
         self.button_选择文档 = self.page.locator('.cheetah-upload button')
         self.button_保存 = self.page.locator(".op-btn-outter-content", has_text="存草稿").locator("button")
         self.上传文档成功提示 = self.page.get_by_text("导入成功")
