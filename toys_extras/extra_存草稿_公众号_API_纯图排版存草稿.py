3160b7d75a5a54790d3a251cf98803d6b53ab864
toysextra <toysextra@tutamail.com>
2025-10-23 12:06:50 +0800

    Update extra_存草稿_公众号_API_纯图排版存草稿.py

--- toys_extras/extra_存草稿_公众号_API_纯图排版存草稿.py
+++ toys_extras/extra_存草稿_公众号_API_纯图排版存草稿.py
@@ -6,7 +6,7 @@ import random
 import shutil
 from natsort import natsorted
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 
 class Toy(Base, MarkdownToHtmlConverter):
@@ -35,6 +35,10 @@ class Toy(Base, MarkdownToHtmlConverter):
         appid = self.config.get("扩展", "appid")
         secret = self.config.get("扩展", "secret")
         是否存稿 = True if self.config.get("扩展", "是否存稿") == "是" else False
+        作者 = self.config.get("扩展", "作者")
+        原文链接 = self.config.get("扩展", "原文链接")
+        留言开关 = True if self.config.get("扩展", "留言开关") == "是" else False
+        是否粉丝才可留言 = True if self.config.get("扩展", "是否粉丝才可留言") == "是" else False
         输出文件格式 = "txt" if self.config.get("扩展", "输出文件格式 -- 可填txt或html") not in ["txt", "html"] else self.config.get("扩展", "输出文件格式 -- 可填txt或html")
         排版输出目录 = self.config.get("扩展", "排版输出目录")
         完成后移动文件到指定文件夹 = self.config.get("扩展", "完成后移动文件到指定文件夹")
@@ -95,14 +99,24 @@ class Toy(Base, MarkdownToHtmlConverter):
                 title = os.path.basename(dir_name)
             else:
                 title = title[0]
-            res = wechat_api.save_draft([{
+            article = {
                 "title": title,
                 "content": html_content,
-                "thumb_media_id": wechat_api.add_thumb(files[0])
-            }])
+                "thumb_media_id": wechat_api.add_thumb(files[0]),
+            }
+            if 作者:
+                article["author"] = 作者
+            if 原文链接:
+                article["content_source_url"] = 原文链接
+            if 留言开关:
+                article["need_open_comment"] = 留言开关
+            if 是否粉丝才可留言:
+                article["only_fans_can_comment"] = 是否粉丝才可留言
+            res = wechat_api.save_draft([article])
             if "errmsg" in res:
                 self.result_table_view.append([dir_name, "失败", res["errmsg"]])
             else:
                 self.result_table_view.append([dir_name, "成功", ""])
             if 完成后移动文件到指定文件夹:
                 shutil.move(dir_name, os.path.join(完成后移动文件到指定文件夹, dir_name))  # type: ignore
+
